b997cb81f557048a354cb85d70b9faa7d6c57f06
wangjian <wangjian02@sobey.com>
2025-08-07 15:59:02 +0800

    🎨 1.更新博客项目架构

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -5,9 +5,9 @@ export const head: HeadConfig[] = [
   ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
   ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
   ["meta", { name: "msapplication-TileColor", content: "#000000" }],
-  ["meta", { name: "msapplication-TileImage", content: "/blog/favicon.ico" }],
+  ["meta", { name: "msapplication-TileImage", content: "/favicon.ico" }],
   ["meta", { name: "baidu-site-verification", content: "codeva-Whjnr38WFE" }],
-  ["link", { rel: "apple-touch-icon", href: "/blog/favicon.ico" }],
-  ["link", { rel: "mask-icon", href: "/blog/favicon.ico", color: "#3eaf7c" }],
-  ["link", { rel: "manifest", href: "/blog/manifest.webmanifest" }],
+  ["link", { rel: "apple-touch-icon", href: "/favicon.ico" }],
+  ["link", { rel: "mask-icon", href: "/favicon.ico", color: "#3eaf7c" }],
+  ["link", { rel: "manifest", href: "/manifest.webmanifest" }],
 ];
